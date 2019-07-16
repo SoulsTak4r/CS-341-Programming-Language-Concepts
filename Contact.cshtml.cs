aa0f986baba6b9082119d950ea9cbf1e6035e9f1
Fnu Nousheerwan <sheeryspeedy@hotmail.com>
2019-07-16 13:56:00 -0500

    Add files via upload

--- Contact.cshtml.cs
+++ Contact.cshtml.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace crimes.Pages
+namespace program.Pages
 {
     public class ContactModel : PageModel
     {
